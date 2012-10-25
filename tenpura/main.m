6fa0f415fced3314fa3cc56d08acf9924b72aafe
y.uchida <user@USER-no-Mac-mini.local>
2012-10-26 00:46:26 +0900

    Initial Commit

---
+++ tenpura/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  tenpura
+//
+//  Created by y.uchida on 12/10/26.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
