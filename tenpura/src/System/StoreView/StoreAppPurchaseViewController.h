9b536fdc4780fca369e79c5912528a25d94035aa
y.uchida <kuronokurujp@gmail.com>
2012-11-25 03:44:46 +0900

    課金ビューコントーラー追加

---
+++ tenpura/src/System/StoreView/StoreAppPurchaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  StoreAppPurchaseViewController.h
+//  tenpura
+//
+//  Created by y.uchida on 12/11/25.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StoreAppPurchaseViewController : UIViewController
+
+@end
---
+++ tenpura/src/System/StoreView/StoreAppPurchaseViewController.m
@@ -0,0 +1,38 @@
+//
+//  StoreAppPurchaseViewController.m
+//  tenpura
+//
+//  Created by y.uchida on 12/11/25.
+//
+//
+
+#import "StoreAppPurchaseViewController.h"
+
+@interface StoreAppPurchaseViewController ()
+
+@end
+
+@implementation StoreAppPurchaseViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
